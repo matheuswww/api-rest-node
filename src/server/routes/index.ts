c115dde888a66422b053cd2363f91fe23b030e6b
virussv <teteus.30.08.07@outlook.com>
2023-03-15 08:00:59 -0300

    feat: added request validation

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { CidadesController } from './../controllers';
 
 const router = Router();
 
-router.post('/teste/',(req,res) => {
-  console.log(req.body);
-  return res.status(StatusCodes.ACCEPTED).json(req.body);
+router.get('/',(_,res) => {
+  return res.send('Olá,DEV!');
 });
+
+router.post('/cidades',CidadesController.createValidation,CidadesController.create);
 
 export { router };
