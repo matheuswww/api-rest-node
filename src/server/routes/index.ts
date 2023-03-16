5c72afe37b50cc2fd21ce134ff9c43477e2b7923
virussv <teteus.30.08.07@outlook.com>
2023-03-16 07:55:58 -0300

    feat: jest configured

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,6 +7,10 @@ router.get('/',(_,res) => {
   return res.send('Olá,DEV!');
 });
 
+router.get('/cidades',CidadesController.getAllValidation,CidadesController.getValidation);
+router.get('/cidades/:id',CidadesController.getByidValidation,CidadesController.getById);
 router.post('/cidades',CidadesController.createValidation,CidadesController.create);
+router.put('/cidades/:id',CidadesController.updateByIdvalidation,CidadesController.updateById);
+router.delete('/cidades/:id',CidadesController.deleteByIdValidation,CidadesController.deleteById);
 
 export { router };
